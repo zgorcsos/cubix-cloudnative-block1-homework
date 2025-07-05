2edebee7d1082b8ae2d86f49c613af74de9e7243
Görcsös Zoltán <gorcsos.zoltan@gmail.com>
2025-07-05 18:12:44 +0200

    Modifying the test script

--- verify-image.sh
+++ verify-image.sh
@@ -2,7 +2,7 @@
 
 docker run -d --name test -p 8080:8080 ghcr.io/zgorcsos/cubix/1/homework/app:springboot > /dev/null
 sleep 10
-curl --fail http://localhost:8080/application/test
+curl --fail http://localhost:8080/cubix/test
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
   echo "Verification failed, here are the logs"
